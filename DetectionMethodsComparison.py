1d9cd63815c4df5d823b36664c7f9afa5a939e4d
Panos Sysk <Pansysk75@gmail.com>
2022-06-02 15:19:51 +0300

    Refined comparisons

--- DetectionMethodsComparison.py
+++ DetectionMethodsComparison.py
@@ -1,17 +1,16 @@
 import random
+
+import numpy as np
 from Point import Point
 from HexGenerator import HexGridGenerator
 from Detector import MLD, ThrassosDetector
 from Detector1 import Detector1
 from Detector2 import Detector2
-from Detector3 import Detector3
-from time import time
+from time import time_ns
 
 from Polygon import Polygon
 from scipy.spatial import Voronoi
 from math import sqrt
-
-from matplotlib.colors import rgb2hex
 from matplotlib import pyplot as plt
 
 ##################################
@@ -21,7 +20,7 @@ from matplotlib import pyplot as plt
 
 
 def draw_point(point, plt: plt, col="black"):
-    plt.scatter(point.x, point.y, color=col, s=1)
+    plt.scatter(point.x, point.y, color=col, s=0.1)
 
 
 def draw_polygon(polygon: Polygon, plt: plt):
@@ -30,19 +29,11 @@ def draw_polygon(polygon: Polygon, plt: plt):
              [polygon[0][i][1] for i in range(polygon.nPoints())])
 
 
-def draw_lines_from_indices(indices, d_min, plt):
-    # Draws three lines that correspond
-    i0, i1, i2 = indices[0], indices[1], indices[2]
-    b = d_min/2
-    a = 2*b/sqrt(3)
-    offset1 = 3*a/2
-    offset2 = a/2
-    k0, k1, k2 = b, a, a
-
-    plt.axline((i0*k0, 0), (i0*k0, 1))
-    plt.axline(((i1*k1 - offset1)*sqrt(3), 0), (0, k1*i1 - offset1))
-    plt.axline(((k2*i2 - offset2)*sqrt(3), 0),
-               (0, (k2*i2 - offset2)))
+def draw_polygon_outline(polygon: Polygon, plt: plt):
+    coordinates = polygon[0]
+    coordinates.append(coordinates[0])
+    xs, ys = zip(*coordinates)
+    plt.plot(xs, ys, color="black", linewidth=0.5)
 
 
 def get_voronoi_polygons(points):
@@ -60,8 +51,11 @@ def get_voronoi_polygons(points):
 
     return voronoi_polygons
 
-    def add_gaussian_noise(self, point, n0):
-        return Point(point.x + np.random.normal(loc=0.0, scale=sqrt(n0/2)), point.y + np.random.normal(loc=0.0, scale=sqrt(n0/2)))
+
+def add_gaussian_noise(point, n0):
+    return Point(point.x + np.random.normal(loc=0.0, scale=sqrt(n0/2)),
+                 point.y + np.random.normal(loc=0.0, scale=sqrt(n0/2)))
+
 
 ##################
 # implementation #
@@ -70,7 +64,8 @@ def get_voronoi_polygons(points):
 
 d_min = 0.7
 
-m_values = [16*(2**i) for i in range(1, 8)]
+
+m_values = [16*(2**i) for i in range(2, 9)]
 results = list()
 for m in m_values:
     gen = HexGridGenerator(d_min)
@@ -79,44 +74,61 @@ for m in m_values:
     voronoi_polygons = get_voronoi_polygons(points)
 
     detector1 = Detector1(d_min, points)
-    detector2 = Detector2(d_min, points, voronoi_polygons,
-                          max(4, int(sqrt(m)/4)))
+    # detector2 = Detector2(d_min, points, voronoi_polygons,
+    #                      max(4, int(sqrt(m)/4)))
+    detector2 = Detector2(d_min, points, voronoi_polygons, 64, 6*d_min)
     thrassos_detector = ThrassosDetector(points, gen)
     mld_detector = MLD(points)
 
-    detectors = [detector1, detector2, thrassos_detector]
-
-    avg_times = [None for _ in detectors]
+    detectors = [detector1, detector2, thrassos_detector, mld_detector]
 
     points_list = list(points)
-    test_points = [random.choice(points_list) for _ in range(100000)]
+
+    SNR = 30
+
+    n0 = gen.compute_av_energy(points) / (10**(SNR / 10))
+    test_points = [add_gaussian_noise(random.choice(
+        points_list), n0) for _ in range(10000)]
+
+    time_avg = [None for _ in detectors]
 
     for i, detector in enumerate(detectors):
+        if detector == mld_detector and len(points) > 512:
+            time_avg[i] = 10**9
+            # skips MLD for very large constellations
+            continue
 
         time_sum = 0
+        t0 = time_ns()
         for point in test_points:
-            t0 = time()
-            detector.detect(point)
-            t1 = time()
-            time_sum += (t1-t0)
+            s = detector.detect(point)
+            if detector == detector1 and s[0] == "ERR":
+                detector2.detect(point)
+                t1 = time_ns()
+        t1 = time_ns()
+        if t1-t0 == 0:
+            print("Time error")
+        dt = t1-t0
+        time_avg[i] = (t1-t0)/len(test_points)
 
-        avg_times[i] = time_sum/len(test_points)
-
-    results.append(avg_times)
+    results.append(time_avg)
 
 
-plt.plot(m_values, [item[0]*1000 for item in results], label="method 1")
-plt.plot(m_values, [item[1]*1000 for item in results], label="method 2")
-plt.plot(m_values, [item[2]*1000 for item in results], label="thrassos")
-# plt.plot(m_values, [item[3] for item in results])
+plt.plot(
+    m_values, [item[0]/1000 for item in results], label="method 1")
+plt.plot(
+    m_values, [item[1]/1000 for item in results], label="method 2")
+plt.plot(
+    m_values, [item[2]/1000 for item in results], label="thrassos")
+plt.plot(
+    m_values, [item[3]/1000 for item in results], label="MLD")
+
+
+plt.title("High SNR time comparison")
 plt.xlabel("Constellation size")
-plt.ylabel("Average symbol detection time (ms)")
+plt.ylabel("Average symbol detection time (μs)")
+plt.ylim([0, 100])
 # plt.xscale("log")
-
 plt.legend()
-
-
+plt.savefig('SNR_30.png', dpi=300)
 plt.show()
-print(results)
-
-# plt.show()
